142c2ebb142a088e5d6c4c3895c63232c1778b24
Dan Stadler <dan.stadler@grafana.com>
2025-02-19 15:12:04 -0800

    first try at takehome setup

---
+++ pre-provision/takehome/terraform/gke.tf
@@ -0,0 +1,154 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+
+# GKE cluster
+data "google_container_engine_versions" "gke_version" {
+  location = var.region
+  version_prefix = "1.30."
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "zzz-stadler-tf-take-home"
+  location = var.region
+  deletion_protection = false
+
+  node_locations = [
+    "us-central1-a",
+  ]
+
+  monitoring_config {
+	enable_components = []     ## see discussion here: https://github.com/hashicorp/terraform-provider-google/issues/15056
+	managed_prometheus {
+		enabled = false
+	}
+	advanced_datapath_observability_config {
+		enable_metrics = false
+		enable_relay = false
+	}
+  }
+
+  logging_config {
+	enable_components = []
+  }
+
+  # to save time during TF runs, I leave this false, then later resize the default pool to 0 via the UI
+  remove_default_node_pool = false
+  initial_node_count       = 1
+
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+}
+
+# Separately Managed Node Pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-nodes"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  version = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
+
+  node_count = 4
+  autoscaling {
+	min_node_count = "4"
+	max_node_count = "4"
+  }
+  # max_pods_per_node = "0"
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+	  "https://www.googleapis.com/auth/devstorage.read_only",
+	  "https://www.googleapis.com/auth/service.management.readonly",
+	  "https://www.googleapis.com/auth/servicecontrol",
+	  "https://www.googleapis.com/auth/trace.append"
+    ]
+
+    labels = {
+      env = var.project_id
+    }
+
+    # preemptible  = true
+    machine_type = "n1-standard-1"
+    tags         = ["node-group", "primary-nodes"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
+
+resource "google_container_node_pool" "big_nodes" {
+  name       = "big-nodes"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  version = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
+
+  node_count = 2
+  autoscaling {
+	min_node_count = "2"
+	max_node_count = "2"
+  }
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+	  "https://www.googleapis.com/auth/devstorage.read_only",
+	  "https://www.googleapis.com/auth/service.management.readonly",
+	  "https://www.googleapis.com/auth/servicecontrol",
+	  "https://www.googleapis.com/auth/trace.append"
+    ]
+
+    labels = {
+      env = var.project_id
+    }
+
+    # preemptible  = true
+    machine_type = "n1-standard-4"
+    tags         = ["node-group", "big-nodes"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
+
+
+
+
+# variable "gke_num_nodes" {
+#   default     = 2
+#   description = "number of gke nodes"
+# }
+
+
+# variable "gke_username" {
+#   default     = ""
+#   description = "gke username"
+# }
+
+# variable "gke_password" {
+#   default     = ""
+#   description = "gke password"
+# }
+
+
+
+
+# # Kubernetes provider
+# # The Terraform Kubernetes Provider configuration below is used as a learning reference only. 
+# # It references the variables and resources provisioned in this file. 
+# # We recommend you put this in another file -- so you can have a more modular configuration.
+# # https://learn.hashicorp.com/terraform/kubernetes/provision-gke-cluster#optional-configure-terraform-kubernetes-provider
+# # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/tutorials/terraform/kubernetes-provider.
+
+# provider "kubernetes" {
+#   load_config_file = "false"
+
+#   host     = google_container_cluster.primary.endpoint
+#   username = var.gke_username
+#   password = var.gke_password
+
+#   client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
+#   client_key             = google_container_cluster.primary.master_auth.0.client_key
+#   cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+# }
