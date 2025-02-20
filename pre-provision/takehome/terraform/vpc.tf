02ea51ffb50b5813a3f7e2db6b360175c4702181
Dan Stadler <dan.stadler@grafana.com>
2025-02-19 16:13:06 -0800

    add TF files to .gitignore

--- pre-provision/takehome/terraform/vpc.tf
+++ pre-provision/takehome/terraform/vpc.tf
@@ -16,13 +16,13 @@ provider "google" {
 
 # VPC
 resource "google_compute_network" "vpc" {
-  name                    = "zzz-stadler-tf-take-home-vpc"
+  name                    = "my-takehome-vpc"
   auto_create_subnetworks = "false"
 }
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
-  name          = "zzz-stadler-tf-take-home-vpc-subnet"
+  name          = "my-takehome-vpc"
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
